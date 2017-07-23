778d47023d1782400f423fe0ea5d4bfcdae0bef1
Travis CI User <travis@example.org>
2017-07-23 23:39:05 +0000

    Documentation upload

---
+++ pyo3/typeob/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["initialize_type","Register new type in python object system."]],"trait":[["PyObjectAlloc","A Python object allocator that is usable as a base type for #[class]"],["PyTypeInfo","Python type information."],["PyTypeObject","Trait implemented by Python object types that have a corresponding type object."]]});
